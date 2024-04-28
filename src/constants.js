793365a4435d13f13ea4ac6db7568603fadbe7a0
jakob <berentsenjakob@gmail.com>
2024-04-27 21:45:56 -0700

    updated dialog information

--- src/constants.js
+++ src/constants.js
@@ -7,11 +7,11 @@ export const dialogueData = {
   "sofa-table": `That's my sofa. I like to relax here and watch TV and spend quality time with my family.
     <button onclick="copyTextToClipboard('berentsenjakob@gmail.com')">Copy Email</button>`,
   tv: `That's my TV. I've been watching tech YouTubers a lot recently like :
-    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@CodeMonkeyUnity" target="_blank">Melkey</a>!`,
+    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@_JohnHammond" target="_blank"> John Hammond</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> and <a href="https://www.youtube.com/@CodeMonkeyUnity" target="_blank">CodeMonkey</a>!`,
   bed: `This where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JohnnyHackz" target="_blank">Check it out?</a>
-    Contact me at <button onclick="copyTextToClipboard('berentsenjakob@gmail.com')">Copy Email</button> if you have any interesting job opportunities!`,
+    Contact me at <button onclick="copyTextToClipboard('berentsenjakob@gmail.com')">berentsenjakob@gmail.com</button> if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. </a>.`,
   library: `There are a lot of programming and cyber books on my shelves.
