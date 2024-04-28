2a6aa5835635785cfee289fecb57aa877fd194ac
jakob <berentsenjakob@gmail.com>
2024-04-27 23:22:33 -0700

    changes to constants.js buttons

--- src/constants.js
+++ src/constants.js
@@ -6,14 +6,13 @@ export const dialogueData = {
 
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It taught me many things but none as important as how to commit and finish something. There was many times I wanted to quit but I didn't. It was a very theoretical degree but I think it gave me a good foundation.`,
 
-  "sofa-table": `That's my sofa. I like to relax here and watch TV and spend quality time with my family.
-    <button id="copyEmailSofa">Copy Email</button>`,
+  "sofa-table": `That's my sofa. I like to relax here and watch TV and spend quality time with my family.`,
 
   tv: `That's my TV. I've been watching tech YouTubers a lot recently like:
     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@_JohnHammond" target="_blank"> John Hammond</a>,
     <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> and <a href="https://www.youtube.com/@CodeMonkeyUnity" target="_blank">CodeMonkey</a>!`,
 
-  bed: `This where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  bed: `This where I sleep. I like my bed! It keeps my mind sharp and most of my great ideas come to me during my dreams.`,
 
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JohnnyHackz" target="_blank">Check it out?</a>
     Contact me at <button id="copyEmailResume">berentsenjakob@gmail.com</button> if you have any interesting job opportunities!`,
